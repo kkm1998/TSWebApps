0f3b2b1390deb31b37f6da9e69adae0e377a6de8
kkm1998 <50990017+kkm1998@users.noreply.github.com>
2020-04-30 10:29:55 +0200

    Delete App.ts

--- FormCreator/TS/App.ts
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', ()=>new App())
-class App{
-    constructor(){
-        let x =new Form('Studia')
-       x.render()
-        document.getElementById('Send')?.addEventListener('click',()=>{x.createTable()})
-
-    }
-}
