3516725e3d2026298ce6f6821a5c372aff052eee
kkm1998 <50990017+kkm1998@users.noreply.github.com>
2020-04-30 11:30:43 +0200

    Delete main.js

--- FormCreator/dist/JS/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var FieldType;
-(function (FieldType) {
-    FieldType["textBox"] = "text";
-    FieldType["Check"] = "checkbox";
-    FieldType["Email"] = "email";
-    FieldType["Select"] = "select";
-    FieldType["TextArea"] = "textarea";
-})(FieldType || (FieldType = {}));
