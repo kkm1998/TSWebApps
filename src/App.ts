9f1f1e25abb40f8ec8c27bf7409192485b0a590d
kkm1998 <50990017+kkm1998@users.noreply.github.com>
2020-06-12 10:40:31 +0200

    Add files via upload

---
+++ src/App.ts
@@ -0,0 +1,8 @@
+import { Form } from './Form'
+import './styles/styles.scss';
+document.addEventListener('DOMContentLoaded', () => new App())
+class App {
+    constructor() {
+        let formStudia = new Form('Studia')
+    }
+}
