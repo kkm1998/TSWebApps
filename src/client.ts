31ca29898ef9b2d016156964d6a72b34dc2c6aae
kkm1998 <50990017+kkm1998@users.noreply.github.com>
2020-06-12 10:48:14 +0200

    Delete client.ts

--- src/client.ts
+++
@@ -1,6 +0,0 @@
-       let socket = new WebSocket("ws://localhost:8080");
-        let x = document.getElementById('send') as HTMLElement
-        x.addEventListener('click', () => {
-            socket.send('Witaj')
-        })
-
