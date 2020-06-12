4693f15c946468aea87fd24f1cc3b2b581042ad9
kkm1998 <50990017+kkm1998@users.noreply.github.com>
2020-06-12 10:47:19 +0200

    Delete App.ts

--- src/App.ts
+++
@@ -1,8 +0,0 @@
-import { Form } from './Form'
-import './styles/styles.scss';
-document.addEventListener('DOMContentLoaded', () => new App())
-class App {
-    constructor() {
-        let formStudia = new Form('Studia')
-    }
-}
